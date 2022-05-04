a0e5cf6eed11d0e651847a87fbad0df9b3410b9d
mimimi-miOmi-mimi <mimihoho1004@gmail.com>
2022-05-05 00:10:38 +0900

    現在地表示処理追加

--- GoogleMapDemo/MapView.swift
+++ GoogleMapDemo/MapView.swift
@@ -2,8 +2,40 @@ import SwiftUI
 import GoogleMaps
 
 struct MapView: UIViewRepresentable {
+    final class Coordinator: NSObject, CLLocationManagerDelegate {
+        var parent: MapView
+
+        init(mapView: MapView) {
+            parent = mapView
+        }
+
+        func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+            let userLocation = locations.last
+            guard let latitude = userLocation?.coordinate.latitude, let longitude = userLocation?.coordinate.longitude else { return }
+            let camera = GMSCameraPosition.camera(
+                withLatitude: latitude,
+                longitude: longitude,
+                zoom: 15.0
+            )
+            parent.mapView.animate(to: camera)
+            parent.locationManager.stopUpdatingLocation()
+        }
+    }
+
     let mapView = GMSMapView(frame: .zero)
+    @State var locationManager = CLLocationManager()
+
+    func makeCoordinator() ->Coordinator {
+        return Coordinator(mapView: self)
+    }
+
     func makeUIView(context: Context) -> some UIView {
+        mapView.isMyLocationEnabled = true
+        locationManager.delegate = context.coordinator
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.distanceFilter = 50
+        locationManager.startUpdatingLocation()
         return mapView
     }
 
